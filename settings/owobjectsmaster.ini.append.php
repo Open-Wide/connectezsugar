f18f1111609f8aa141bd62170144df7e8615911d
Massimiliano Pasquesi <massimiliano.pasquesi@openwide.fr>
2012-02-14 11:34:05 +0000

    petite correction dans owobjectsmaster.ini

---
+++ settings/owobjectsmaster.ini.append.php
@@ -0,0 +1,15 @@
+<?php /* 
+
+[Users]
+# ID de l'Admin User
+AdminID=14
+# je sais pas si c'est utile ....
+#DefaultGroupID=112
+
+[Tree]
+# ID du node parent par default pour la création des objets
+DefaultParentNodeID=2
+# ID de la section par default pour la création des objets
+DefaultSectionID=1
+
+*/ ?>
