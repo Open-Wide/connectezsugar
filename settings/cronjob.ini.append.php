c1b735a55f434ab61d37ba20a3e17a308000f598
Massimiliano Pasquesi <massimiliano.pasquesi@openwide.fr>
2012-02-23 17:45:11 +0000

    chronjob de cleanup massif

--- settings/cronjob.ini.append.php
+++ settings/cronjob.ini.append.php
@@ -4,12 +4,17 @@
 [CronjobSettings]
 ExtensionDirectories[]=connectezsugar
 Scripts[]=synchronize_ezsugar.php
+Scripts[]=cleanup_dev.php
 
 
 [CronjobPart-synchro]
 Scripts[]=synchronize_ezsugar.php
 
+[CronjobPart-cleanup]
+Scripts[]=cleanup_dev.php
 
-#php runcronjobs.php synchro
+
+# php runcronjobs.php synchro
+# php runcronjobs.php cleanup
 
 */ ?>
