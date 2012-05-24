d77198c21b28df52272095b38f6e1692f2bebff6
Loïc Martinez <loic.martinez@openwide.fr>
2012-05-24 04:41:25 +0000

    pour moins d'utilisation mémoire

--- settings/sugarcrm.ini.append.php
+++ settings/sugarcrm.ini.append.php
@@ -133,7 +133,7 @@ mapping_types[relate]=ezobjectrelation
 modulesListToSynchro[]
 #modulesListToSynchro[]=otcp_contact
 #modulesListToSynchro[]=otcp_company
-modulesListToSynchro[]=otcp_culture
+#modulesListToSynchro[]=otcp_culture
 modulesListToSynchro[]=otcp_accommodation
 modulesListToSynchro[]=otcp_restaurant
 modulesListToSynchro[]=otcp_transport
