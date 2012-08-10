81cb6004c642011f13ae6285c9af98d13f5e929b
Fabien Jarnet <fabien.jarnet@openwide.fr>
2012-08-10 15:09:38 +0000

    Ajout du type de relation (one ou list)

--- settings/mappingezsugarschema.ini.append.php
+++ settings/mappingezsugarschema.ini.append.php
@@ -6,18 +6,22 @@ relation_to_attribute[]
 [otcp_culture]
 relation_to_attribute[]
 relation_to_attribute[many_cult_one_comp]=acti_company
+relation_to_attribute_type[many_cult_one_comp]=one
 
 [otcp_accommodation]
 relation_to_attribute[]
 relation_to_attribute[many_acco_one_comp]=acti_company
+relation_to_attribute_type[many_acco_one_comp]=one
 
 [otcp_transport]
 relation_to_attribute[]
 relation_to_attribute[many_tran_one_comp]=acti_company
+relation_to_attribute_type[many_tran_one_comp]=one
 
 [otcp_shopping]
 relation_to_attribute[]
 relation_to_attribute[many_shop_one_comp]=acti_company
+relation_to_attribute_type[many_shop_one_comp]=one
 
 [otcp_company]
 relation_to_attribute[]
@@ -25,10 +29,12 @@ relation_to_attribute[]
 [otcp_visit]
 relation_to_attribute[]
 relation_to_attribute[many_visi_one_comp]=acti_company
+relation_to_attribute_type[many_visi_one_comp]=one
 
 [otcp_otherpro]
 relation_to_attribute[]
 relation_to_attribute[many_othe_one_comp]=acti_company
+relation_to_attribute_type[many_othe_one_comp]=one
 
 [otcp_room]
 relation_to_attribute[]
@@ -36,5 +42,6 @@ relation_to_attribute[]
 [otcp_restaurant]
 relation_to_attribute[]
 relation_to_attribute[many_rest_one_comp]=acti_company
+relation_to_attribute_type[many_rest_one_comp]=one
 
 */ ?>
