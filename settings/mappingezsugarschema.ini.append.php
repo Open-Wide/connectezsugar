018a23d8eb2374efdfdd51dbce84c3ce94fa9a24
Fabien Jarnet <fabien.jarnet@openwide.fr>
2012-08-13 15:24:26 +0000

    Ajout de tous les champs medias pour les activités +  Réorganisation des fiches

--- settings/mappingezsugarschema.ini.append.php
+++ settings/mappingezsugarschema.ini.append.php
@@ -3,48 +3,66 @@
 [otcp_contact]
 relation_to_attribute[]
 
+[otcp_company]
+relation_to_attribute[]
+
 [otcp_culture]
 relation_to_attribute[]
 relation_to_attribute[many_cult_one_comp]=acti_company
 relation_to_attribute_type[many_cult_one_comp]=one
-specific_attribute[]=media
+specific_attribute[]=medias
 specific_attribute_media[type]=ezobjectrelationlist
-specific_attribute_media[name]=Média
+specific_attribute_media[name]=Médias
 
 [otcp_accommodation]
 relation_to_attribute[]
 relation_to_attribute[many_acco_one_comp]=acti_company
 relation_to_attribute_type[many_acco_one_comp]=one
+specific_attribute[]=medias
+specific_attribute_media[type]=ezobjectrelationlist
+specific_attribute_media[name]=Médias
 
 [otcp_transport]
 relation_to_attribute[]
 relation_to_attribute[many_tran_one_comp]=acti_company
 relation_to_attribute_type[many_tran_one_comp]=one
+specific_attribute[]=medias
+specific_attribute_media[type]=ezobjectrelationlist
+specific_attribute_media[name]=Médias
 
 [otcp_shopping]
 relation_to_attribute[]
 relation_to_attribute[many_shop_one_comp]=acti_company
 relation_to_attribute_type[many_shop_one_comp]=one
-
-[otcp_company]
-relation_to_attribute[]
+specific_attribute[]=medias
+specific_attribute_media[type]=ezobjectrelationlist
+specific_attribute_media[name]=Médias
 
 [otcp_visit]
 relation_to_attribute[]
 relation_to_attribute[many_visi_one_comp]=acti_company
 relation_to_attribute_type[many_visi_one_comp]=one
+specific_attribute[]=medias
+specific_attribute_media[type]=ezobjectrelationlist
+specific_attribute_media[name]=Médias
 
 [otcp_otherpro]
 relation_to_attribute[]
 relation_to_attribute[many_othe_one_comp]=acti_company
 relation_to_attribute_type[many_othe_one_comp]=one
-
-[otcp_room]
-relation_to_attribute[]
+specific_attribute[]=medias
+specific_attribute_media[type]=ezobjectrelationlist
+specific_attribute_media[name]=Médias
 
 [otcp_restaurant]
 relation_to_attribute[]
 relation_to_attribute[many_rest_one_comp]=acti_company
 relation_to_attribute_type[many_rest_one_comp]=one
+specific_attribute[]=medias
+specific_attribute_media[type]=ezobjectrelationlist
+specific_attribute_media[name]=Médias
+
+[otcp_room]
+relation_to_attribute[]
 
 */ ?>
