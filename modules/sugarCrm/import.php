d33362733a2acdf991309d43f14c8ae2fcae1025
Fabien Jarnet <fabien.jarnet@openwide.fr>
2012-09-14 07:25:40 +0000

    Gestion du bouton de synchro vers eZ

--- modules/sugarCrm/import.php
+++ modules/sugarCrm/import.php
@@ -1,5 +1,76 @@
 <?php
+
+/*
+ * NEW VERSION / Fabien
+ * */
+
 include_once( 'kernel/common/template.php' );
+include_once( 'extension/connectezsugar/classes/Module_Schema.php' );
+include_once( 'extension/connectezsugar/classes/Module.php' );
+include_once( 'extension/connectezsugar/classes/Module_Object.php' );
+
+$sugar_module = $Params[ 'sugarmodule' ];
+$sugar_id 	  = $Params[ 'sugarid' ];
+$notice       = null;
+$result       = null;
+
+if( is_null( $sugar_module ) or is_null( $sugar_id ) ) {
+	$result[ ] = 'sugar_module et/ou remote_id manquant !!!';
+} else {
+
+	$ez_identifier = Module_Schema::get_ez_class_identifier( $sugar_module );
+	
+	eZDebug::writeNotice( 'remote_id = ' . $ez_identifier . '_' . $sugar_id );
+	
+	try {
+		$sugarConnector = new SugarConnector( );
+		$connection     = $sugarConnector->login( );
+	} catch ( Exception $e ) {
+		$result[ ] = $e->getMessage( );
+	}
+	$select_fields  = Module::load_include_fields( $sugar_module );
+	$sugardata 		= $sugarConnector->get_entry( $sugar_module, $sugar_id, $select_fields );
+	$num_item       = 1;
+	$cli            = null;
+	$simulation     = false;
+	
+	if (isset ( $sugardata[ 'data' ] ) ) {
+		try {
+			$schema = new Module_Schema( $sugar_module, $cli );
+			$object = new Module_Object( $sugar_module, $sugar_id, $schema, $cli, $simulation, $num_item );
+			$object->update( $sugardata );
+			$notice[ ] = implode( '<br />', $object->logs );
+			unset( $object, $sugardata );
+		} catch ( Exception $e ) {
+			$result[ ] = $e->getMessage( );
+		}
+	} else {
+		$result[ ] = 'Aucune donnée récupérée par get_entry() pour ID=' . $sugar_id . ' et sugar_module=' . $sugar_module;
+		$result[] = var_dump($sugardata);
+	}
+}
+
+$tpl = templateInit( );
+$tpl->setVariable( 'result', $result );
+$tpl->setVariable( 'notice', $notice );
+$tpl->setVariable( 'title', 'Import d\'un objet SUGAR' );
+$tpl->setVariable( 'subtitle', 'Creation / Update d\'objets existants' );
+$Result = array( );
+$Result['content']    = $tpl->fetch( 'design:sugarcrm/notice_result.tpl' );
+$Result['pagelayout'] = false;
+$Result['path']       = array(
+	array(
+		'url'  => false,
+		'text' => 'import',
+	)
+);
+
+
+/*
+ * OLD VERSION
+ */
+
+/*include_once( 'kernel/common/template.php' );
 include_once( 'extension/connectezsugar/scripts/genericfunctions.php' );
 
 if(isset($Params["sugarmodule"]))
@@ -165,14 +236,14 @@ else
 					$continue = false;
 					
 					// OPTION 2 : si un attribute SUGAR n'existe pas chez EZ on le crée
-					/*$objectsMaster->setProperty('class_id',$ezclassID);
-					foreach($verif as $attr)
-					{
-						$newattr[] = $objectsMaster->createClassAttribute($attr);
-						$result[] = "nouveu attribut avec identifier " . $attr['identifier'] . " crée pou la class " . $ez_properties['class_identifier'];
-					}
+					//$objectsMaster->setProperty('class_id',$ezclassID);
+					//foreach($verif as $attr)
+					//{
+					//	$newattr[] = $objectsMaster->createClassAttribute($attr);
+					//	$result[] = "nouveu attribut avec identifier " . $attr['identifier'] . " crée pou la class " . $ez_properties['class_identifier'];
+					//}
 					
-					$continue = true;*/
+					//$continue = true;
 				}
 				else
 				{
@@ -297,6 +368,6 @@ $Result['content'] = $tpl->fetch( 'design:sugarcrm/notice_result.tpl' );
 $Result['pagelayout']=false;
 $Result['path'] = array( array( 'url' => false,
                                'text' => 'import' ) );
-
+*/
 ?>
 
