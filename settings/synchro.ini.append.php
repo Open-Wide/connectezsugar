6042f2580acb63bf8321ceb0533011bb821098a9
Fabien Jarnet <fabien.jarnet@openwide.fr>
2012-09-27 09:27:07 +0000

    Application de la date de dernière synchro objets + relations

--- settings/synchro.ini.append.php
+++ settings/synchro.ini.append.php
@@ -1,41 +1,41 @@
 <?php /* #?ini charset="utf-8"?
 
 [Synchro]
-lastSynchroDatetime=2012-09-10 09:00:00
+lastSynchroDatetime=2012-09-26 06:00:00
 
 [export_module]
-last_synchro_otcp_accommodation=2012-08-11 15:00:00
-last_synchro_otcp_company=2012-08-11 15:00:00
-last_synchro_otcp_contact=2012-08-11 15:00:00
-last_synchro_otcp_culture=2012-08-11 15:00:00
-last_synchro_otcp_otherpro=2012-08-11 15:00:00
-last_synchro_otcp_restaurant=2012-08-11 15:00:00
-last_synchro_otcp_room=2012-08-11 15:00:00
-last_synchro_otcp_shopping=2012-08-11 15:00:00
-last_synchro_otcp_transport=2012-08-11 15:00:00
-last_synchro_otcp_visit=2012-08-11 15:00:00
+last_synchro_otcp_accommodation=2012-09-26 06:00:00
+last_synchro_otcp_company=2012-09-26 06:00:00
+last_synchro_otcp_contact=2012-09-26 06:00:00
+last_synchro_otcp_culture=2012-09-26 06:00:00
+last_synchro_otcp_otherpro=2012-09-26 06:00:00
+last_synchro_otcp_restaurant=2012-09-26 06:00:00
+last_synchro_otcp_room=2012-09-26 06:00:00
+last_synchro_otcp_shopping=2012-09-26 06:00:00
+last_synchro_otcp_transport=2012-09-26 06:00:00
+last_synchro_otcp_visit=2012-09-26 06:00:00
 
 [import_module]
-last_synchro_otcp_accommodation=2012-08-11 15:00:00
-last_synchro_otcp_company=2012-08-11 15:00:00
-last_synchro_otcp_contact=2012-08-11 15:00:00
-last_synchro_otcp_culture=2012-08-11 15:00:00
-last_synchro_otcp_otherpro=2012-08-11 15:00:00
-last_synchro_otcp_restaurant=2012-08-11 15:00:00
-last_synchro_otcp_room=2012-08-11 15:00:00
-last_synchro_otcp_shopping=2012-08-11 15:00:00
-last_synchro_otcp_transport=2012-08-11 15:00:00
-last_synchro_otcp_visit=2012-08-11 15:00:00
+last_synchro_otcp_accommodation=2012-09-26 06:00:00
+last_synchro_otcp_company=2012-09-26 06:00:00
+last_synchro_otcp_contact=2012-09-26 06:00:00
+last_synchro_otcp_culture=2012-09-26 06:00:00
+last_synchro_otcp_otherpro=2012-09-26 06:00:00
+last_synchro_otcp_restaurant=2012-09-26 06:00:00
+last_synchro_otcp_room=2012-09-26 06:00:00
+last_synchro_otcp_shopping=2012-09-26 06:00:00
+last_synchro_otcp_transport=2012-09-26 06:00:00
+last_synchro_otcp_visit=2012-09-26 06:00:00
 
 [import_module_relations]
-last_synchro_otcp_accommodation=2012-08-11 15:00:00
-last_synchro_otcp_company=2012-08-11 15:00:00
-last_synchro_otcp_contact=2012-08-11 15:00:00
-last_synchro_otcp_culture=2012-08-11 15:00:00
-last_synchro_otcp_otherpro=2012-08-11 15:00:00
-last_synchro_otcp_restaurant=2012-08-11 15:00:00
-last_synchro_otcp_room=2012-08-11 15:00:00
-last_synchro_otcp_shopping=2012-08-11 15:00:00
-last_synchro_otcp_transport=2012-08-11 15:00:00
-last_synchro_otcp_visit=2012-08-11 15:00:00
+last_synchro_otcp_accommodation=2012-09-26 06:00:00
+last_synchro_otcp_company=2012-09-26 06:00:00
+last_synchro_otcp_contact=2012-09-26 06:00:00
+last_synchro_otcp_culture=2012-09-26 06:00:00
+last_synchro_otcp_otherpro=2012-09-26 06:00:00
+last_synchro_otcp_restaurant=2012-09-26 06:00:00
+last_synchro_otcp_room=2012-09-26 06:00:00
+last_synchro_otcp_shopping=2012-09-26 06:00:00
+last_synchro_otcp_transport=2012-09-26 06:00:00
+last_synchro_otcp_visit=2012-09-26 06:00:00
 */ ?>
