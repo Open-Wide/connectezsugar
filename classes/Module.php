ee55a8d2af3f1e98a7c5ea4031c196aefaf39f43
Fabien Jarnet <fabien.jarnet@openwide.fr>
2013-02-21 13:39:56 +0000

    Gestion par lot de get_entry_list

--- classes/Module.php
+++ classes/Module.php
@@ -95,11 +95,30 @@ class Module extends Module_Object_Accessor {
 		$remote_ids    = array( ); // Tableau qui contiendra les ID CRM modifiés, donc à récupérer côté eZ
 		$select_fields = array( 'id' );
 		$offset 	   = 0;
-		$max_results   = 7500;
+		$max_results   = 99999;
 		$query 		   = '';
 		$order_by	   = '';
 		$deleted	   = false;
-		$remotedata     = $this->connector->get_entry_list( $this->module_name, $select_fields, $offset, $max_results, $query, $order_by, $deleted );
+		
+		$nb_par_lot = 250;
+		$remotedata = array('data' => array());
+		for ( $offset = 0; $offset < $max_results; $offset += $nb_par_lot) {
+			$data = $this->connector->get_entry_list( $this->module_name, $select_fields, $offset, $nb_par_lot, $query, $order_by, $deleted );
+			if ( is_array( $data ) && is_array( $data[ 'data' ] ) && count( $data[ 'data' ] ) > 0 ) {
+				$remotedata['data'] = array_merge( $remotedata['data'], $data['data'] );
+				$this->notice('[OFFSET ' . $offset . '] get_entry_list [memory=' . memory_get_usage_hr() . ']');
+				unset($data);
+			} else {
+				if ( !is_array( $data ) || !is_array( $data['data'] ) ) {
+					$this->warning(print_r($data));
+					$this->error( 'arret de la fonction check() avec offset = ' . $offset . ' [memory=' . memory_get_usage_hr() . ']');
+					return false;
+				}
+				$this->notice('get_entry_list terminé');
+				// Plus de données à renvoyer, on sort de la boucle
+				break;
+			}
+		}
 		
 		$count_remote_data = 0;
 		if (is_array($remotedata) && isset($remotedata[ 'data' ])) {
@@ -171,7 +190,7 @@ class Module extends Module_Object_Accessor {
 					$object_found = eZContentObject::fetchByRemoteID( $class_identifier.'_'.$diff_remote_id );
 					if ( $object_found ) {
 						$this->notice( '[' . $cpt . '] ' . $diff_remote_id . ' | Suppression de ' . $object_found->mainNodeID() . ' - ' . $object_found->Name );
-						//$object_found->mainNode()->removeThis();
+						$object_found->mainNode()->removeThis();
 					} else {
 						$this->notice( '[' . $cpt . '] ' . $diff_remote_id . ' Objet introuvable dans le CRM');
 					}
@@ -345,7 +364,25 @@ class Module extends Module_Object_Accessor {
 		$max_results   = 99999;
 		$query 		   = $this->module_name . '.date_modified >= "' . strftime( '%Y-%m-%d %H:%M:%S', $timestamp ) . '"';
 		$order_by	   = '';
-		$remotedata    = $this->connector->get_entry_list( $this->module_name, $select_fields, $offset, $max_results, $query, $order_by, $deleted );
+		
+		$nb_par_lot = 250;
+		$remotedata = array('data' => array());
+		for ( $offset = 0; $offset < $max_results; $offset += $nb_par_lot) {
+			$data = $this->connector->get_entry_list( $this->module_name, $select_fields, $offset, $nb_par_lot, $query, $order_by, $deleted );
+			if ( is_array( $data ) && is_array( $data[ 'data' ] ) && count( $data[ 'data' ] ) > 0 ) {
+				$this->notice('[OFFSET ' . $offset . '] get_entry_list [memory=' . memory_get_usage_hr() . ']');
+				$remotedata['data'] = array_merge( $remotedata['data'], $data['data'] );
+			} else {
+				if ( !is_array( $data ) || is_array( $data['data'] ) ) {
+					$this->error( 'arret de la fonction check() avec offset = ' . $offset . ' [memory=' . memory_get_usage_hr() . ']');
+					return false;
+				}
+				$this->notice('get_entry_list terminé');
+				// Plus de données à renvoyer, on sort de la boucle
+				break;
+			}
+			unset($data);
+		}
 		
 		if ( isset( $remotedata['data'] ) ) {
 			foreach ( $remotedata['data'] as $data ) {
