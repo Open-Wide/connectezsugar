d8553dde0ce820535af797ba78df71365361eb52
Fabien Jarnet <fabien.jarnet@openwide.fr>
2013-03-11 09:04:51 +0000

    Plus besoin de paramètre obligatoire : "sync" par défaut (pour utilisation dans le cronjob daily)

--- cronjobs/import.php
+++ cronjobs/import.php
@@ -18,26 +18,21 @@ $cli->beginout("import.php");
 
 $arguments = array_slice( $_SERVER['argv'], 1 );
 
-if ( !isset( $arguments[ 1 ] ) ) {
-	$cli->error( "Usage:    php runcronjobs.php import <mode> " );
-	$cli->error( "Example:  php runcronjobs.php import [ sync | simul ]" );
-} else {
-	// connexion à SUGAR
-	$sugarConnector = new SugarConnector();
-	$connection = $sugarConnector->login();
+// connexion à SUGAR
+$sugarConnector = new SugarConnector();
+$connection = $sugarConnector->login();
 	
-	$simulation = $arguments[ 1 ];
+$simulation = isset($arguments[ 1 ]) ? $arguments[ 1 ] : 'sync';
  
-	// modules SUGAR à synchroniser
-	$modules_list = SugarSynchro::getModuleListToSynchro();
-	$cli->gnotice("Mémoire utilisée avant boucle sur les modules : " . memory_get_usage_hr());
+// modules SUGAR à synchroniser
+$modules_list = SugarSynchro::getModuleListToSynchro();
+$cli->gnotice("Mémoire utilisée avant boucle sur les modules : " . memory_get_usage_hr());
 	
-	foreach ( $modules_list as $sugarmodule ) {
-	    $cli->gnotice("Mémoire utilisée debut import module $sugarmodule : " . memory_get_usage_hr());
-	    exec("php runcronjobs.php --debug --logfiles importmodule $sugarmodule $simulation");  
-	    gc_collect_cycles();
-	    $cli->gnotice("Mémoire utilisée fin import module : " . memory_get_usage_hr());
-	}
+foreach ( $modules_list as $sugarmodule ) {
+    $cli->gnotice("Mémoire utilisée debut import module $sugarmodule : " . memory_get_usage_hr());
+    exec("php runcronjobs.php --debug --logfiles importmodule $sugarmodule $simulation");  
+    gc_collect_cycles();
+    $cli->gnotice("Mémoire utilisée fin import module : " . memory_get_usage_hr());
 }
 
 
--- cronjobs/synchronize_relations.php
+++ cronjobs/synchronize_relations.php
@@ -11,26 +11,21 @@ $cli->beginout("synchronize_relations.php");
 
 $arguments = array_slice( $_SERVER['argv'], 1 );
 
-if ( !isset( $arguments[ 1 ] ) ) {
-	$cli->error( "Usage:    php runcronjobs.php importrelations <mode> " );
-	$cli->error( "Example:  php runcronjobs.php importrelations [ sync | simul ]" );
-} else {
-	// connexion à SUGAR
-	$sugarConnector = new SugarConnector();
-	$connection     = $sugarConnector->login();
+// connexion à SUGAR
+$sugarConnector = new SugarConnector();
+$connection     = $sugarConnector->login();
 	
-	$simulation     = $arguments[ 1 ];
+$simulation 	= isset($arguments[ 1 ]) ? $arguments[ 1 ] : 'sync';
  
-	// modules SUGAR à synchroniser
-	$modules_list = SugarSynchro::getModuleListToSynchro();
-	$cli->gnotice("Mémoire utilisée avant boucle sur les modules : " . memory_get_usage_hr());
+// modules SUGAR à synchroniser
+$modules_list = SugarSynchro::getModuleListToSynchro();
+$cli->gnotice("Mémoire utilisée avant boucle sur les modules : " . memory_get_usage_hr());
 	
-	foreach ( $modules_list as $sugarmodule ) {
-	    $cli->gnotice("Mémoire utilisée debut import relations module $sugarmodule : " . memory_get_usage_hr());
-	    exec("php runcronjobs.php --debug --logfiles importrelationsmodule $sugarmodule $simulation");  
-	    gc_collect_cycles();
-	    $cli->gnotice("Mémoire utilisée fin import relations module : " . memory_get_usage_hr());
-	}
+foreach ( $modules_list as $sugarmodule ) {
+    $cli->gnotice("Mémoire utilisée debut import relations module $sugarmodule : " . memory_get_usage_hr());
+    exec("php runcronjobs.php --debug --logfiles importrelationsmodule $sugarmodule $simulation");  
+    gc_collect_cycles();
+    $cli->gnotice("Mémoire utilisée fin import relations module : " . memory_get_usage_hr());
 }
 
 $cli->endout("synchronize_relations.php");
